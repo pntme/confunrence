4a42125ac0667fe0c44fb5e3f1e6ca6b556f1d83
prabhu <prabhunath877@gmail.com>
2017-03-30 11:12:16 +0530

    login page completed

--- client/src/pages/login/login.component.ts
+++ client/src/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-
 import { NavController } from 'ionic-angular';
+import { FacebookAuth, GoogleAuth, User } from '@ionic/cloud-angular';
+
 
 @Component({
   selector: 'login-page',
@@ -8,8 +9,27 @@ import { NavController } from 'ionic-angular';
 })
 export class LoginPage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public facebookAuth: FacebookAuth, public navCtrl: NavController, public googleAuth: GoogleAuth, public user: User) {
     
   }
+
+
+ GoFb(){
+ 	this.facebookAuth.login().then((res)=>{
+       console.log(res)
+ 	}, (e)=>{
+ 		console.log(e)
+ 	});
+ 	console.log('want to login with fb');
+ }
+ 
+ GoGmail(){
+ 	console.log('want to login with gmail');		
+ 	this.googleAuth.login().then((res)=> {
+ 		console.log(res)
+ 	}, (e)=>{
+ 		console.log(e);
+ 	});																																																																																																																									
+ }
 
 }
