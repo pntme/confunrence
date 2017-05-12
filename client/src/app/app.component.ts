308e92f6c2e50125df8470f1349300b9a702877a
prabhu <prabhunath877@gmail.com>
2017-05-12 13:05:15 +0530

    again syncing for pulling profile component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -59,10 +59,11 @@ export class MyApp {
     });
   }
 
-  openPage(page) {
+  openPage(component) {
+    console.log(component)
       // Reset the content nav to have just this page
       // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+    this.nav.setRoot(component);
   }
 
   logout(){
